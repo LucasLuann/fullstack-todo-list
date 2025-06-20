1b66b18ce905ca53fb86f8fc7a2ba08d11592378
Lucas Luann <lucasluann10@gmail.com>
2025-06-20 15:45:50 -0400

    chore: instalando router dom

--- client/vite-project/src/App.tsx
+++ client/vite-project/src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoList from "./pages/TodoList";
 
 function App() {
-  return <TodoList />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<h1>Página não encontrada</h1>} />
+        <Route path="/" element={<TodoList />} />
+        <Route path="/tarefas" element={<TodoList />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
